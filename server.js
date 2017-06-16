d04a67bd15d884d7b2d442aff60e80187b6c0602
gnoompa <gnoompa@gmail.com>
2017-06-16 04:38:58 +0300

    test

---
+++ server.js
@@ -0,0 +1,11 @@
+var fs = require('fs')
+var express = require('express')
+var app = express()
+
+app.use('/dist', express.static(`${__dirname}/dist`))
+
+app.get('/', function (req, res) {
+	res.sendFile(`${__dirname}/index.html`)
+})
+
+app.listen(process.env.PORT || 8085, function () { console.log('Server has been started!') })
